fdb4ccac15b24047cfe3632a47778f63a826604d
GNz11 <angelokimhui@gmail.com>
2022-04-19 03:52:19 +0800

    Revert "BACKPORT: smp, ARM64: Kill SMP single function call interrupt"
    This reverts commit 7763d0adea153912d2529a7b99585cf87de5196a.

--- arch/arm64/kernel/smp.c
+++ arch/arm64/kernel/smp.c
@@ -67,6 +67,7 @@ volatile unsigned long secondary_holding_pen_release = INVALID_HWID;
 enum ipi_msg_type {
 	IPI_RESCHEDULE,
 	IPI_CALL_FUNC,
+	IPI_CALL_FUNC_SINGLE,
 	IPI_CPU_STOP,
 	IPI_TIMER,
 	IPI_IRQ_WORK,
@@ -513,7 +514,7 @@ void arch_send_call_function_ipi_mask(const struct cpumask *mask)
 
 void arch_send_call_function_single_ipi(int cpu)
 {
-	smp_cross_call_common(cpumask_of(cpu), IPI_CALL_FUNC);
+	smp_cross_call_common(cpumask_of(cpu), IPI_CALL_FUNC_SINGLE);
 }
 
 void arch_send_wakeup_ipi_mask(const struct cpumask *mask)
@@ -525,6 +526,7 @@ static const char *ipi_types[NR_IPI] __tracepoint_string = {
 #define S(x,s)	[x] = s
 	S(IPI_RESCHEDULE, "Rescheduling interrupts"),
 	S(IPI_CALL_FUNC, "Function call interrupts"),
+	S(IPI_CALL_FUNC_SINGLE, "Single function call interrupts"),
 	S(IPI_CPU_STOP, "CPU stop interrupts"),
 	S(IPI_TIMER, "Timer broadcast interrupts"),
 	S(IPI_IRQ_WORK, "IRQ work interrupts"),
@@ -687,6 +689,12 @@ void handle_IPI(int ipinr, struct pt_regs *regs)
 	case IPI_CALL_FUNC:
 		irq_enter();
 		generic_smp_call_function_interrupt();
+		irq_exit();
+		break;
+
+	case IPI_CALL_FUNC_SINGLE:
+		irq_enter();
+		generic_smp_call_function_single_interrupt();
 		irq_exit();
 		break;
 
